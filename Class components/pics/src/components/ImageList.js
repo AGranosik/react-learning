e95324c0966b625655efada454900d390e1e39d8
AGranosik <adrian.granosik@gmail.com>
2019-04-01 22:33:55 +0200

    display iamges with using key property

---
+++ Class components/pics/src/components/ImageList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ImageList = (props) => {
+    const images = props.images.map(({ description, urls, id}) => {
+        return (
+        <img alt={description} key={id} src={urls.regular} />
+        );
+    });
+
+    return <div>{images}</div>;
+};
+
+export default ImageList;

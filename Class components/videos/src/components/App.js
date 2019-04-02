d84db3e9285057304795dbd18aba99b05260f92e
AGranosik <adrian.granosik@gmail.com>
2019-04-03 00:39:36 +0200

    YT app finished

--- Class components/videos/src/components/App.js
+++ Class components/videos/src/components/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component{
+
+    state = { videos: [], selectedVideo: null };
+
+    componentDidMount(){
+        this.onSearchSubmit('city');
+    }
+
     onSearchSubmit = async term => {
         const response = await youtube.get(
             '/search',
@@ -12,13 +21,30 @@ class App extends React.Component{
                 },
             }
         );
-        console.log(response);
+        this.setState({ 
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+         });
     }
+
+    onVideoSelect = (video) => {
+        this.setState({ selectedVideo: video });
+    };
 
     render(){
         return(
             <div className="ui container">
                 <SearchBar onSubmit={this.onSearchSubmit}/>
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }

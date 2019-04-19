ecd746570ab775671b6ab4c9d9cecd4fbdd9b489
AGranosik <adrian.granosik@gmail.com>
2019-04-19 20:47:26 +0200

    cleanup and language selector

---
+++ Context/translate/src/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+class App extends React.Component{
+
+    state = { language: 'English' };
+
+    onLanguageChange = language => {
+        this.setState({ language });
+    }
+
+    render(){
+        return(
+            <div className="ui container">
+                <div>
+                    Select a language
+                    <i className="flag us" onClick={ () => this.onLanguageChange('english')} />
+                    <i className="flag nl" onClick={ () => this.onLanguageChange('dutch')} />
+                </div>
+            </div>
+        );
+    }
+}
+
+
+export default App;
